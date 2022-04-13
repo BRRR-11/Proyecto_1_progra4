2d08caebd94055a9ed65e668a675f8c1104a26d7
ArianaQ21 <quesadaariana514@gmail.com>
2022-04-12 18:24:27 -0600

    js

--- Proyecto_1/web/js/TablaPaciente.js
+++ Proyecto_1/web/js/TablaPaciente.js
@@ -1,6 +1,32 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
- */
+var TablaP = Table || {};
 
+TablaP.refresh= function(table){
+    body = table.tBodies[0];
+    while (body.firstChild) body.removeChild(body.firstChild);
+    model = retrieve(table.modelId);
+    rows= TablaP.toRows(model,table.toCell);
+    for (i=0;i<rows.length;i++) body.appendChild(rows[i]);    
+};
 
+TablaP.toRows= function(objs, toCell){
+    rows =  new Array();
+    for (i=0;i<objs.size();i++){
+      obj = objs.get(i);
+      row=TablaP.toRow(obj,toCell,i);
+      rows.push(row);
+    }
+    return rows;
+};
+
+TablaP.toRow= function(obj,toCell,rowIdx){	
+    var row=document.createElement("tr");
+    for(var colIdx=0;;colIdx++){
+        var cellContent=toCell(obj,colIdx);
+        if (cellContent===null) break;
+        var cell=document.createElement("td");
+        cellContent.rowIdx=rowIdx;
+        cell.appendChild(cellContent);
+        row.appendChild(cell);
+    }
+    return row;
+};
---
+++ Proyecto_1/web/js/Utils.js
@@ -0,0 +1,9 @@
+get = function(id){
+    return document.getElementById(id).value;
+};
+
+set = function(id,value){
+    document.getElementById(id).value=value;
+};
+
+
